b58495c85a0c18e496c16c860d2751b7fd7c72ea
kuangbin <kuangbin@kuangbin.com>
2019-09-26 19:11:29 +0800

    增加接口调用

--- COM.JZB.PROJECT/COM.JZB.ORGANIZE.MODULE/src/main/java/com/jzb/org/api/auth/AuthApi.java
+++ COM.JZB.PROJECT/COM.JZB.ORGANIZE.MODULE/src/main/java/com/jzb/org/api/auth/AuthApi.java
@@ -1,15 +1,27 @@
 package com.jzb.org.api.auth;
 
 
+import com.jzb.base.message.Response;
 import org.springframework.cloud.openfeign.FeignClient;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+import java.util.Map;
 
 
 @FeignClient(name = "jzb-auth")
 @RequestMapping(value = "/auth")
 public interface AuthApi {
-
+    /**
+     * 注册的第二步操作创建用户
+     *
+     * @param param
+     * @return
+     */
+    @PostMapping("/addRegistration")
+    @CrossOrigin
+    public Response addRegistration(@RequestBody Map<String, Object> param);
 }
