0911557b4389dea624fe0a64f0a99ca634ecba34
20180509-161608\Administrator <13326964756@163.com>
2019-09-27 20:06:29 +0800

    合同配置中的产品参数

--- COM.JZB.PROJECT/COM.JZB.RESOURCE.MOUDLE/src/main/java/com/jzb/resource/service/TbProductResListService.java
+++ COM.JZB.PROJECT/COM.JZB.RESOURCE.MOUDLE/src/main/java/com/jzb/resource/service/TbProductResListService.java
@@ -1,5 +1,6 @@
 package com.jzb.resource.service;
 
+import com.jzb.base.util.JzbRandom;
 import com.jzb.resource.dao.TbProductResListMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,8 +13,6 @@ public class TbProductResListService {
 
     @Autowired
     private TbProductResListMapper tbProductResListMapper;
-
-
 
     /**
      * 查询模板功能信息
@@ -31,5 +30,81 @@ public class TbProductResListService {
      */
     public int addProductRes(Map<String, Object> param){
         return tbProductResListMapper.addProductResList(param);
+    }
+
+    /**
+     * 查询合同配置中的产品参数
+     *    根据产品线的id查询产品表
+     *    * 根据产品id查询出产品参数
+     * @param param
+     * @return
+     */
+    public List<Map<String, Object>> getProductResList(Map<String, Object> param) {
+        return tbProductResListMapper.getProductResList(param);
+
+    }
+
+
+    /**
+     * 根据产品的名称获取产品参数列表
+     * @param param
+     * @return
+     */
+    public List<Map<String, Object>> getProductResListCname(Map<String, Object> param) {
+        return tbProductResListMapper.getProductResListCname(param);
+    }
+
+    /**
+     * 合同配置中产品参数中新建中查询出产品名称
+     * @param param
+     * @return
+     */
+    public List<Map<String, Object>> queryProductListCname(Map<String, Object> param) {
+        return tbProductResListMapper.queryProductListCname(param);
+    }
+
+
+    /*
+     * 3.获取排序
+     * */
+    public int getTbProductParameteItemIdx() {
+        return tbProductResListMapper.getTbProductParameteItemIdx();
+    }
+
+    /**
+     * 添加新建产品参数
+     * @param paramList
+     * @return
+     */
+    public int saveTbProductParameteItem(List<Map<String, Object>> paramList) {
+        return tbProductResListMapper.saveTbProductParameteItem(paramList);
+    }
+
+    /**
+     * 修改合同配置中的产品参数列表
+     * @param param
+     * @return
+     */
+    public int updateTbProductParameteItem(List<Map<String, Object>> paramList) {
+        return tbProductResListMapper.updateTbProductParameteItem(paramList);
+    }
+
+    /**
+     * 添加资源产品表的数据
+     *
+     * @param param
+     * @return
+     */
+    public int saveTbProductResList(Map<String, Object> param) {
+        return tbProductResListMapper.saveTbProductResList(param);
+    }
+
+    /**
+     * 修改资源产品表中的数据
+     * @param param
+     * @return
+     */
+    public int updateTbProductResList(Map<String, Object> param) {
+      return tbProductResListMapper.updateTbProductResList(param);
     }
 }
