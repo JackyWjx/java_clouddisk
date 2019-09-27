b61bd5d3f206da8abefb2ae5b95a64cd44e2f087
kuangbin <kuangbin@kuangbin.com>
2019-09-27 19:21:21 +0800

    优化代码

--- COM.JZB.PROJECT/COM.JZB.CONFIG.MODULE/src/main/java/com/jzb/config/service/TbCityService.java
+++ COM.JZB.PROJECT/COM.JZB.CONFIG.MODULE/src/main/java/com/jzb/config/service/TbCityService.java
@@ -78,7 +78,38 @@ public class TbCityService {
      * @author kuangbin
      */
     public Map<String, Object> getRegionInfo(Map<String, Object> param) {
+        // 加入查询状态
         param.put("status", 1);
-        return tbCityMapper.queryRegionInfo(param);
+        Map<String, Object> region = tbCityMapper.queryRegionInfo(param);
+        Map<String, Object> name = new HashMap<>();
+        // 判断市级是否为空
+        if (JzbDataType.isEmpty(JzbDataType.getString(region.get("city")))){
+            // 为空直接返回查询的结果
+            region.put("level1",JzbDataType.getString(region.get("creaid")));
+        // 判断县级是否为空
+        } else if (JzbDataType.isEmpty(JzbDataType.getString(region.get("county")))){
+            // 为空加入省级名称查询省级ID
+            name.put("regionName",JzbDataType.getString(region.get("province")));
+            name = getRegionID(name);
+            // 加入省级ID
+            region.put("level1",JzbDataType.getString(name.get("creaid")));
+
+            // 加入查询到的市级ID
+            region.put("level2",JzbDataType.getString(region.get("creaid")));
+        }else {
+            // 都不为空查询省级ID和市级ID
+            String regionName = JzbDataType.getString(region.get("province"))+"-"+JzbDataType.getString(region.get("city"));
+            name.put("regionName",regionName);
+            // 查询市级
+            name = getRegionID(name);
+            region.put("level2",JzbDataType.getString(name.get("creaid")));
+            name.put("regionName",JzbDataType.getString(region.get("province")));
+            // 查询省级
+            name = getRegionID(name);
+            region.put("level1",JzbDataType.getString(name.get("creaid")));
+            // 加入查询到的县级
+            region.put("level3",JzbDataType.getString(region.get("creaid")));
+        }
+        return region;
     }
 }
