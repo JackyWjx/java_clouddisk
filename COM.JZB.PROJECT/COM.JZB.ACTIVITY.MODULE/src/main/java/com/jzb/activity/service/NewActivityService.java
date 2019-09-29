9591cfc2d9f41174e5f7c864bdaa276cd181b6a6
kuangbin <kuangbin@kuangbin.com>
2019-09-29 19:26:37 +0800

    新增文章列表查询,新建

--- COM.JZB.PROJECT/COM.JZB.ACTIVITY.MODULE/src/main/java/com/jzb/activity/service/NewActivityService.java
+++ COM.JZB.PROJECT/COM.JZB.ACTIVITY.MODULE/src/main/java/com/jzb/activity/service/NewActivityService.java
@@ -25,84 +25,95 @@ public class NewActivityService {
 
     /**
      * 获取详情
+     *
      * @param param
      * @return
      */
-    public List<Map<String ,Object>> getActivityDesc(Map<String ,Object> param){
+    public List<Map<String, Object>> getActivityDesc(Map<String, Object> param) {
         return activityMapper.getDiscussById(param);
     }
 
     /**
      * 分页查询 支显示3条信息
+     *
      * @param params
-     * @return List<Map<String, Object>> 返回list数据
+     * @return List<Map < String, Object>> 返回list数据
      */
-    public List<Map<String, Object>> findParticularsList(Map<String, Object> params){
+    public List<Map<String, Object>> findParticularsList(Map<String, Object> params) {
         return activityMapper.findParticularsList(params);
     }
 
     /**
      * 分页查询详情页面
+     *
      * @param params
-     * @return List<Map<String, Object>> 返回list数据
+     * @return List<Map < String, Object>> 返回list数据
      */
-    public List<Map<String, Object>> findParticularsByList(Map<String, Object> params){
+    public List<Map<String, Object>> findParticularsByList(Map<String, Object> params) {
         return activityMapper.findParticularsByList(params);
     }
 
     /**
      * 查询总数
+     *
      * @param params
      * @return
      */
-    public int findParticularsListCount(Map<String, Object> params){
+    public int findParticularsListCount(Map<String, Object> params) {
         return activityMapper.findParticularsListCount(params);
     }
 
     /**
      * 查询总数
+     *
      * @param params
      * @return
      */
-    public int addActivityDucess(Map<String, Object> params){
-        Long addtime=System.currentTimeMillis();
-        params.put("addtime",addtime);
-        params.put("disid", params.get("actid").toString()+JzbRandom.getRandomCharCap(6));
-        params.put("status",1);
+    public int addActivityDucess(Map<String, Object> params) {
+        Long addtime = System.currentTimeMillis();
+        params.put("addtime", addtime);
+        params.put("disid", params.get("actid").toString() + JzbRandom.getRandomCharCap(6));
+        params.put("status", 1);
         return activityMapper.addActivityDiscuss(params);
     }
 
     /**
      * 无登录点赞
+     *
      * @param param
      * @return
      */
-    public int updateActivityVotes(Map<String, Object> param){
+    public int updateActivityVotes(Map<String, Object> param) {
         return activityMapper.updateActivityVotes(param);
     }
 
     /**
      * 无登录点赞
+     *
      * @param param
      * @return
      */
-    public int updateActivityReads(Map<String, Object> param){
+    public int updateActivityReads(Map<String, Object> param) {
         return activityMapper.updateActivityReads(param);
     }
 
     /**
      * 查询点赞次数浏览次数
+     *
      * @param param
      * @return
      */
-    public  Map<String, Object> getVotesViews(Map<String, Object> param){return  activityMapper.queryActivityCountNew(param);}
+    public Map<String, Object> getVotesViews(Map<String, Object> param) {
+        return activityMapper.queryActivityCountNew(param);
+    }
 
     /**
      * 更新评论数
+     *
      * @param param
      * @return
      */
-    public int updateComment(Map<String, Object> param){
+    public int updateComment(Map<String, Object> param) {
         return activityMapper.updateComment(param);
     }
 
@@ -156,5 +167,41 @@ public class NewActivityService {
         param.put("pageno", (pageno - 1) * pagesize);
         param.put("pagesize", pagesize);
         return param;
+    }
+
+    /**
+     * CRM-运营管理-活动-文章列表
+     * 点击新建后加入新建的活动文章内容
+     *
+     * @author kuangbin
+     */
+    public int addActivityList(Map<String, Object> param) {
+        long addtime = System.currentTimeMillis();
+        // 获取活动ID
+        String actid = JzbRandom.getRandomCharCap(11);
+        param.put("addtime", addtime);
+        param.put("actid", actid);
+        // 加入文章信息
+        int count = activityMapper.insertActivityList(param);
+        if (count > 0) {
+            Object photo = param.get("photolist");
+            if (JzbDataType.isCollection(photo)) {
+                List<Map<String, Object>> photoList = (List<Map<String, Object>>) photo;
+                for (int i = 0; i < photoList.size(); i++) {
+                    Map<String, Object> photoMap = photoList.get(i);
+                    // 加入默认状态
+                    photoMap.put("status", "1");
+                    photoMap.put("addtime", addtime);
+                    photoMap.put("actid", actid);
+                    photoMap.put("uid", param.get("uid"));
+                    photoMap.put("summary", param.get("summary"));
+                }
+                // 加入活动图片
+                count = activityMapper.insertActivityPhoto(photoList);
+            }else {
+                count = 0;
+            }
+        }
+        return count;
     }
 }
