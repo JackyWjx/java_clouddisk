078a0c0eec607be05fd00f658f0ababa98227576
kuangbin <kuangbin@kuangbin.com>
2019-09-29 14:39:12 +0800

    增加查询文章列表接口

--- COM.JZB.PROJECT/COM.JZB.ACTIVITY.MODULE/src/main/java/com/jzb/activity/service/NewActivityService.java
+++ COM.JZB.PROJECT/COM.JZB.ACTIVITY.MODULE/src/main/java/com/jzb/activity/service/NewActivityService.java
@@ -1,8 +1,12 @@
 package com.jzb.activity.service;
 
+import com.jzb.activity.api.redis.UserRedisApi;
 import com.jzb.activity.dao.ActivityMapper;
+import com.jzb.base.data.JzbDataType;
 import com.jzb.base.data.date.JzbDateStr;
+import com.jzb.base.message.Response;
 import com.jzb.base.util.JzbRandom;
+import com.jzb.base.util.JzbTools;
 import org.apache.poi.ss.formula.ptg.Pxg;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +19,9 @@ public class NewActivityService {
 
     @Autowired
     private ActivityMapper activityMapper;
+
+    @Autowired
+    private UserRedisApi userRedisApi;
 
     /**
      * 获取详情
@@ -97,5 +104,57 @@ public class NewActivityService {
      */
     public int updateComment(Map<String, Object> param){
         return activityMapper.updateComment(param);
+    }
+
+    /**
+     * CRM-运营管理-活动-文章列表
+     * 点击文章列表显示所有的文章列表总数
+     *
+     * @author kuangbin
+     */
+    public int getActivityListCount(Map<String, Object> param) {
+        int count;
+        try {
+            count = activityMapper.queryActivityListCount(param);
+        } catch (Exception ex) {
+            JzbTools.logError(ex);
+            count = 0;
+        }
+        return count;
+    }
+
+    /**
+     * CRM-运营管理-活动-文章列表
+     * 点击文章列表显示所有的文章列表
+     *
+     * @author kuangbin
+     */
+    public List<Map<String, Object>> getActivityList(Map<String, Object> param) {
+        // 设置分页参数
+        param = setPageSize(param);
+        List<Map<String, Object>> list = activityMapper.queryActivityListData(param);
+        for (int i = 0; i < list.size(); i++) {
+            Map<String, Object> advertMap = list.get(i);
+            // 获取企业管理员用户信息
+            String uid = JzbDataType.getString(advertMap.get("uid"));
+            param.put("uid", uid);
+            // 从缓存中获取用户信息
+            Response response = userRedisApi.getCacheUserInfo(param);
+            advertMap.put("uid", response.getResponseEntity());
+        }
+        return list;
+    }
+
+    /**
+     * 设置分页数
+     */
+    public Map<String, Object> setPageSize(Map<String, Object> param) {
+        int pageno = JzbDataType.getInteger(param.get("pageno"));
+        int pagesize = JzbDataType.getInteger(param.get("pagesize"));
+        pagesize = pagesize <= 0 ? 15 : pagesize;
+        pageno = pageno <= 0 ? 1 : pageno;
+        param.put("pageno", (pageno - 1) * pagesize);
+        param.put("pagesize", pagesize);
+        return param;
     }
 }
