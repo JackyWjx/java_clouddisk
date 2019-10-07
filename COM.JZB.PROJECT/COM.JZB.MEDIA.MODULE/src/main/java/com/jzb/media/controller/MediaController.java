b1de5762863295dd404ecc4b94254fcc42260364
hanbin <hanbin@hanbin.com>
2019-10-07 15:35:40 +0800

    邮件配置

--- COM.JZB.PROJECT/COM.JZB.MEDIA.MODULE/src/main/java/com/jzb/media/controller/MediaController.java
+++ COM.JZB.PROJECT/COM.JZB.MEDIA.MODULE/src/main/java/com/jzb/media/controller/MediaController.java
@@ -3,6 +3,7 @@ package com.jzb.media.controller;
 import com.jzb.base.message.Response;
 import com.jzb.base.util.JzbTools;
 import com.jzb.media.service.MediaService;
+import io.undertow.servlet.spec.HttpServletResponseImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -30,37 +31,33 @@ public class MediaController {
 
     @RequestMapping("/upDownload")
     @ResponseBody
-    public void  upDownload(@RequestBody Map<String , Object> paraMap){
+    public void  upDownload(HttpServletRequest req , HttpServletResponse res){
         File file;
         FileInputStream fis = null;
         ServletOutputStream out = null;
-        HttpServletResponse res = null;
+        Map<String , Object> paraMap =  new HashMap<>();
         try{
-            JzbTools.logInfo("fileId ==============>>",paraMap.get("fileid"));
+            JzbTools.logInfo("fileId ==============>>",req.getParameter("fileId"));
+            paraMap.put("fileid",req.getParameter("fileId"));
             List<Map<String , Object>> list = mediaService.queryMedia(paraMap);
             JzbTools.logInfo("path ==============>>",list.get(0).get("filepath"));
             JzbTools.logInfo("fileName ==============>>",list.get(0).get("filename"));
             res.setCharacterEncoding("UTF-8");
             res.setHeader("Content-Disposition", "attachment;filename*=UTF-8''" + URLEncoder.encode(list.get(0).get("filename").toString(),"UTF-8"));
-            file = new File("/opt/jzb/v3/software/nginx/html/static"+list.get(0).get("filepath"));
+            file = new File("/home/V3/jzb/application/nginx/html/static"+list.get(0).get("filepath"));
 //            file = new File("D:\\nginx\\nginx-1.17.3\\nginx-1.17.3\\html\\images\\短信平台相关.xls");
             fis =  new FileInputStream(file);
-            //写出
             out = res.getOutputStream();
-            //定义读取缓冲区
             byte buffer[] = new byte[1024];
-            //定义读取长度
             int len = 1024;
-            //循环读取
             while((len = fis.read(buffer))!=-1){
                 out.write(buffer,0,len);
             }
         }catch (Exception e){
-
+            JzbTools.logError(e);
         }finally {
             try{
                 JzbTools.logInfo("资源 ==============>> 释放");
-                //释放资源
                 fis.close();
                 out.close();
             }catch (Exception e){
