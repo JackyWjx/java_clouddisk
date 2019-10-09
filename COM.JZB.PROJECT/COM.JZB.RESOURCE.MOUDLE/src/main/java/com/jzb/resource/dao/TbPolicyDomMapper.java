2e0d8160f933bc92e49ffeac099adb56328ac103
20180509-161608\Administrator <13326964756@163.com>
2019-10-09 20:30:23 +0800

    运营管理下的政策模块

--- COM.JZB.PROJECT/COM.JZB.RESOURCE.MOUDLE/src/main/java/com/jzb/resource/dao/TbPolicyDomMapper.java
+++ COM.JZB.PROJECT/COM.JZB.RESOURCE.MOUDLE/src/main/java/com/jzb/resource/dao/TbPolicyDomMapper.java
@@ -29,4 +29,37 @@ public interface TbPolicyDomMapper {
      *     查询热门文档
      */
     List<Map<String, Object>> queryHotDom(Map<String, Object> param);
+
+    /**
+     * LBQ
+     *  根据菜单类别进行查询  如果没有菜单类别则进行所有的查询
+     * @param param
+     * @return
+     */
+    List<Map<String, Object>> getTbPolicyDocument(Map<String, Object> param);
+
+    /**
+     * LBQ
+     * 运营管理中政策中内容列表的新建
+     *
+     * @param param
+     * @return
+     */
+    int saveTbPolicyDom(Map<String, Object> param);
+
+    /**
+     * 运营管理政策中内容列表的修改
+     *
+     * @param param
+     * @return
+     */
+    int updatePolicyDom(Map<String, Object> param);
+
+    /**
+     * 设置删除状态
+     *
+     * @param param
+     * @return
+     */
+    int updateDelete(Map<String, Object> param);
 }
--- COM.JZB.PROJECT/COM.JZB.RESOURCE.MOUDLE/src/main/java/com/jzb/resource/dao/TbPolicyTypeMapper.java
+++ COM.JZB.PROJECT/COM.JZB.RESOURCE.MOUDLE/src/main/java/com/jzb/resource/dao/TbPolicyTypeMapper.java
@@ -18,6 +18,40 @@ public interface TbPolicyTypeMapper {
      * @return
      */
      List<Map<String, Object>> queryPolicyType(Map<String, Object> param);
+
+
+    /**
+     * LBQ
+     * 查询运营管理下政策中的菜单类别
+     *
+     * @param
+     * @return
+     */
+    List<Map<String, Object>> getPolicyTypes(Map<String, Object> param);
+
+    /** LBQ
+     * 运营管理下政策模块中的菜单类别新增
+     *
+     * @param param
+     * @return
+     */
+    int savePolicyType(Map<String, Object> param);
+
+    /** LBQ
+     * 运营管理下政策模块中的菜单类别修改
+     *
+     * @param param
+     * @return
+     */
+    int updatePolicyType(Map<String, Object> param);
+
+    /**LBQ
+     * 设置删除状态
+     *
+     * @param param
+     * @return
+     */
+    int updateStatus(Map<String, Object> param);
 }
 
 
