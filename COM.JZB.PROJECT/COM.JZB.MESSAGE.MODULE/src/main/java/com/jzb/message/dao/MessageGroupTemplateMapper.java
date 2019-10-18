2f680365faacbafb990309cb7c99a11fda15175c
Administrator <tangshengjun>
2019-10-18 19:31:11 +0800

    111

--- COM.JZB.PROJECT/COM.JZB.MESSAGE.MODULE/src/main/java/com/jzb/message/dao/MessageGroupTemplateMapper.java
+++ COM.JZB.PROJECT/COM.JZB.MESSAGE.MODULE/src/main/java/com/jzb/message/dao/MessageGroupTemplateMapper.java
@@ -16,35 +16,35 @@ public interface MessageGroupTemplateMapper {
     /**
      * 查询
      */
-    List<Map<String, Object>> queryMsgType(Map<String, Object> map);
+    List<Map<String, Object>> queryMsgGroupTemplate(Map<String, Object> map);
 
     /**
      * 查询总数
      */
-    int queryMsgTypeCount(Map<String, Object> map);
+    int queryMsgGroupTemplateCount(Map<String, Object> map);
 
     /**
      * 模糊查询
      */
-    List<Map<String, Object>> searchMsgType(Map<String, Object> map);
+    List<Map<String, Object>> searchMsgGroupTemplate(Map<String, Object> map);
 
     /**
      * 模糊查询总数
      */
-    int searchMsgTypeCount(Map<String, Object> map);
+    int searchMsgGroupTemplateCount(Map<String, Object> map);
 
     /**
      * 添加
      */
-    int insertMsgType(Map<String, Object> map);
+    int insertMsgGroupTemplate(Map<String, Object> map);
 
     /**
      * 修改
      */
-    int updateMsgType(Map<String, Object> map);
+    int updateMsgGroupTemplate(Map<String, Object> map);
 
     /**
      * 禁用
      */
-    int deleteMsgType(Map<String, Object> map);
+    int removeMsgGroupTemplate(Map<String, Object> map);
 }
