62c2ac2f4b6eaa9976a1b7c801b309df9ffa0e90
chenduan <2514571823qq.com>
2019-10-21 14:30:09 +0800

    交通工具controller czd

---
+++ COM.JZB.PROJECT/COM.JZB.OPERATE.MODULE/src/main/java/com/jzb/operate/controller/TbTravelVehicleController.java
@@ -0,0 +1,45 @@
+package com.jzb.operate.controller;
+
+import com.jzb.base.message.PageInfo;
+import com.jzb.base.message.Response;
+import com.jzb.base.util.JzbTools;
+import com.jzb.operate.service.TbTravelVehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/operate/travelVehicle")
+public class TbTravelVehicleController {
+
+    @Autowired
+    private TbTravelVehicleService tbTravelVehicleService;
+
+    @RequestMapping(value = "/getVehicleList",method = RequestMethod.POST)
+    @ResponseBody
+    @CrossOrigin
+    @Transactional
+    public Response getVehicleList(@RequestBody Map<String, Object> param){
+        Response result;
+        try {
+            // 获取交通工具list
+            List<Map<String, Object>> list = tbTravelVehicleService.queryVehicle();
+            // 获取用户信息返回
+            result=Response.getResponseSuccess((Map<String, Object>) param.get("username"));
+
+            // 定义返回对象pageinfo
+            PageInfo pi=new PageInfo();
+            pi.setList(list);
+            pi.setTotal(list.size());
+
+            result.setPageInfo(pi);
+        }catch (Exception ex){
+            JzbTools.logError(ex);
+            result=Response.getResponseError();
+        }
+        return result;
+    }
+}
