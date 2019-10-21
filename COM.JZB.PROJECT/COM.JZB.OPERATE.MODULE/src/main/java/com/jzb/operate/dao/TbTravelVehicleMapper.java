c6d84e3cd3c759f74ef76b7fe853e31eab449e93
chenduan <2514571823qq.com>
2019-10-21 14:30:28 +0800

    czd

--- COM.JZB.PROJECT/COM.JZB.OPERATE.MODULE/src/main/java/com/jzb/operate/dao/TbTravelVehicleMapper.java
+++ COM.JZB.PROJECT/COM.JZB.OPERATE.MODULE/src/main/java/com/jzb/operate/dao/TbTravelVehicleMapper.java
@@ -3,12 +3,16 @@ package com.jzb.operate.dao;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Map;
 
 @Mapper
 @Repository
 public interface TbTravelVehicleMapper {
 
-     // 查询交通工具
-     String queryCnameById(Map<String, Object> param);
+    // 查询交通工具
+    String queryCnameById(Map<String, Object> param);
+
+    // 查询所有交通工具
+    List<Map<String, Object>> queryVehicle();
 }
--- COM.JZB.PROJECT/COM.JZB.OPERATE.MODULE/src/main/java/com/jzb/operate/service/TbTravelVehicleService.java
+++ COM.JZB.PROJECT/COM.JZB.OPERATE.MODULE/src/main/java/com/jzb/operate/service/TbTravelVehicleService.java
@@ -4,6 +4,7 @@ import com.jzb.operate.dao.TbTravelVehicleMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -19,5 +20,14 @@ public class TbTravelVehicleService {
      */
     public String getTravelName(Map<String,Object> param){
         return tbTravelVehicleMapper.queryCnameById(param);
+    }
+
+    /**
+     * 查询所有交通工具
+     *
+     * @return
+     */
+    public List<Map<String, Object>> queryVehicle(){
+        return tbTravelVehicleMapper.queryVehicle();
     }
 }
