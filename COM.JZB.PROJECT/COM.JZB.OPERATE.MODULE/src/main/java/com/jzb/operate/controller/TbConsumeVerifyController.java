3e3441be0e37db05aca720a24c45b97be5fee182
chenduan <2514571823qq.com>
2019-10-22 19:03:25 +0800

    czd

--- COM.JZB.PROJECT/COM.JZB.OPERATE.MODULE/src/main/java/com/jzb/operate/controller/TbConsumeVerifyController.java
+++ COM.JZB.PROJECT/COM.JZB.OPERATE.MODULE/src/main/java/com/jzb/operate/controller/TbConsumeVerifyController.java
@@ -3,11 +3,13 @@ package com.jzb.operate.controller;
 import com.jzb.base.message.Response;
 import com.jzb.base.util.JzbCheckParam;
 import com.jzb.base.util.JzbTools;
+import com.jzb.operate.api.redis.TbCityRedis;
 import com.jzb.operate.service.TbConsumeVerifyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -21,6 +23,22 @@ public class TbConsumeVerifyController {
 
     @Autowired
     private TbConsumeVerifyService tbConsumeVerifyService;
+
+    @Autowired
+    private TbCityRedis tbCityRedis;
+
+    /**
+     * 根据城市id 获取单挑城市信息
+     * @return
+     */
+    @RequestMapping(value = "/getcity",method = RequestMethod.GET)
+    @ResponseBody
+    public Response getcity(){
+        Map<String, Object> param=new HashMap<>();
+        param.put("key",10020600);
+        Response cityList = tbCityRedis.getCityList(param);
+        return cityList;
+    }
 
     /**
      * 添加出差报销审核记录
