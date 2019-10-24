1223e63b0965eae4450fccb49ac2298d0f5469ea
20180509-161608\Administrator <13326964756@163.com>
2019-10-24 10:20:30 +0800

    添加分页工具类

---
+++ COM.JZB.PROJECT/COM.JZB.ORGANIZE.MODULE/src/main/java/com/jzb/org/util/SetPageSize.java
@@ -0,0 +1,20 @@
+package com.jzb.org.util;
+
+import com.jzb.base.data.JzbDataType;
+
+import java.util.Map;
+
+public class SetPageSize {
+    /**
+     * 设置分页数
+     */
+    public Map<String, Object> setPagenoSize(Map<String, Object> param) {
+        int pageno = JzbDataType.getInteger(param.get("pageno"));
+        int pagesize = JzbDataType.getInteger(param.get("pagesize"));
+        pagesize = pagesize <= 0 ? 15 : pagesize;
+        pageno = pageno <= 0 ? 1 : pageno;
+        param.put("pageno", (pageno - 1) * pagesize);
+        param.put("pagesize", pagesize);
+        return param;
+    }
+}
