56f2b9d70d5cfca938026b175f6a72e3e7f59b4f
hanbin <hanbin@hanbin.com>
2019-10-30 16:42:46 +0800

    销售服务业统计分析

---
+++ COM.JZB.PROJECT/COM.JZB.OPERATE.MODULE/src/main/java/com/jzb/operate/service/TbHandItemService.java
@@ -0,0 +1,71 @@
+package com.jzb.operate.service;
+
+import com.jzb.base.data.JzbDataType;
+import com.jzb.base.util.JzbTools;
+import com.jzb.operate.dao.TbHandItemMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Description: 跟进详情
+ * @Author Han Bin
+ */
+@Service
+public class TbHandItemService {
+
+    @Autowired
+    TbHandItemMapper mapper;
+
+    /**
+     * 获取 服务记录
+     *
+     * @param map
+     * @return
+     */
+    public List<Map<String , Object>> queryTbCompanyService(Map<String , Object> map){
+        int page = JzbDataType.getInteger(map.get("pageno")) == 0 ? 0 : JzbDataType.getInteger(map.get("pageno")) - 1;
+        map.put("pageno", page * JzbDataType.getInteger(map.get("pagesize")));
+        map.put("pagesize", JzbDataType.getInteger(map.get("pagesize")));
+        return mapper.queryTbCompanyService(map);
+    }
+
+    /**
+     * 获取 服务记录
+     *
+     * @param map
+     * @return
+     */
+    public int queryTbCompanyServiceCount(Map<String , Object> map){
+        int page = JzbDataType.getInteger(map.get("pageno")) == 0 ? 0 : JzbDataType.getInteger(map.get("pageno")) - 1;
+        map.put("pageno", page * JzbDataType.getInteger(map.get("pagesize")));
+        map.put("pagesize", JzbDataType.getInteger(map.get("pagesize")));
+        return mapper.queryTbCompanyServiceCount(map);
+    }
+
+    /**
+     * 根据项目id 获取项目跟进详情
+     *
+     * @param proid
+     * @return
+     */
+    public List<Map<String , Object>> queryTbHandleItem(Map<String , Object> map){
+        int page = JzbDataType.getInteger(map.get("pageno")) == 0 ? 0 : JzbDataType.getInteger(map.get("pageno")) - 1;
+        map.put("pageno", page * JzbDataType.getInteger(map.get("pagesize")));
+        map.put("pagesize", JzbDataType.getInteger(map.get("pagesize")));
+        return mapper.queryTbHandleItem(map);
+    }
+
+    /**
+     * 根据项目id 获取项目跟进详情总数
+     *
+     * @param proid
+     * @return
+     */
+    public int  queryTbHandleItemCount(Map<String , Object> map){
+        return mapper.queryTbHandleItemCount(map);
+    }
+
+}
