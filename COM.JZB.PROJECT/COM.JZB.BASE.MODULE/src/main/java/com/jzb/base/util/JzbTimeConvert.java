b97957ace27bb22e3b7ec69b76f7b1429e8adf08
chenduan <2514571823qq.com>
2019-10-31 16:56:37 +0800

    czd

---
+++ COM.JZB.PROJECT/COM.JZB.BASE.MODULE/src/main/java/com/jzb/base/util/JzbTimeConvert.java
@@ -0,0 +1,38 @@
+package com.jzb.base.util;
+
+import com.jzb.base.data.JzbDataType;
+import com.jzb.base.data.date.JzbDateStr;
+import com.jzb.base.data.date.JzbDateUtil;
+
+/**
+ * long转时间格式
+ *  * @author chenzhengduan
+ *  * @date 2019/10/31
+ */
+public class JzbTimeConvert {
+
+    /**
+     * 私有构造方法，不允许实例化
+     */
+    private JzbTimeConvert(){
+
+    }
+
+    /**
+     * 转换时间格式
+     * @param times
+     * @return
+     */
+    public static String ToStringyyyy_MM_dd_HH_mm_ss(Object times){
+       return JzbDateUtil.toDateString(JzbDataType.getLong(times),JzbDateStr.yyyy_MM_dd_HH_mm_ss);
+    }
+
+    /**
+     * 转换时间格式
+     * @param times
+     * @return
+     */
+    public static String ToStringyyyy_MM_dd(Object times){
+        return JzbDateUtil.toDateString(JzbDataType.getLong(times),JzbDateStr.yyyy_MM_dd);
+    }
+}
