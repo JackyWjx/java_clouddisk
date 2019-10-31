b111b16a60907e9004a5a0d04a1b06d4e160c729
kuangbin <kuangbin@kuangbin.com>
2019-10-31 10:43:36 +0800

    增加我服务的业主方法

--- COM.JZB.PROJECT/COM.JZB.OPERATE.MODULE/src/main/java/com/jzb/operate/dao/TbCompanyServiceMapper.java
+++ COM.JZB.PROJECT/COM.JZB.OPERATE.MODULE/src/main/java/com/jzb/operate/dao/TbCompanyServiceMapper.java
@@ -22,4 +22,31 @@ public interface TbCompanyServiceMapper {
      * @return
      */
         List<Map<String, Object>> getProject(Map<String, Object> param);
+
+    /**
+     * CRM-销售业主-我服务的业主-1
+     * 获取所有的我服务的业主的总数
+     *
+     * @Author: Kuang Bin
+     * @DateTime: 2019/10/19
+     */
+    int queryCompanyServiceCount(Map<String, Object> param);
+
+    /**
+     * CRM-销售业主-我服务的业主-1
+     * 获取所有的我服务的业主
+     *
+     * @Author: Kuang Bin
+     * @DateTime: 2019/10/19
+     */
+    List<Map<String, Object>> queryCompanyServiceList(Map<String, Object> param);
+
+    /**
+     * CRM-销售业主-我服务的业主-1
+     * 获取所有的我服务的业主项目的次数
+     *
+     * @Author: Kuang Bin
+     * @DateTime: 2019/10/19
+     */
+    int queryServiceCount(Map<String, Object> param);
 }
