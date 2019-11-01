c75bbaa9f8d3391c85e9117bb2af61a2a26774ea
chenduan <2514571823qq.com>
2019-11-01 14:56:51 +0800

    czd

---
+++ COM.JZB.PROJECT/COM.JZB.OPERATE.MODULE/src/main/java/com/jzb/operate/controller/TbCompanyMethodTargetController.java
@@ -0,0 +1,120 @@
+package com.jzb.operate.controller;
+
+import com.jzb.base.log.JzbLoggerUtil;
+import com.jzb.base.message.PageInfo;
+import com.jzb.base.message.Response;
+import com.jzb.base.util.JzbCheckParam;
+import com.jzb.base.util.JzbTools;
+import com.jzb.operate.service.TbCompanyMethodTargetService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/operate/companyMethodTarget")
+public class TbCompanyMethodTargetController {
+
+    @Autowired
+    private TbCompanyMethodTargetService tbCompanyMethodTargetService;
+
+    /**
+     * 日志记录对象
+     */
+    private final static Logger logger = LoggerFactory.getLogger(TbCompanyMethodTargetController.class);
+
+    /**
+     * 获取方法论目标
+     *
+     * @param param
+     * @return
+     */
+    @RequestMapping(value = "/getCompanyMethodTarget", method = RequestMethod.POST)
+    @ResponseBody
+    @Transactional
+    @CrossOrigin
+    public Response getCompanyMethodTarget(@RequestBody Map<String, Object> param) {
+        Response response;
+        Map<String, Object> userInfo = null;
+        String api = "/activity/getDiscuss";
+        boolean flag = true;
+        try {
+            if (param.get("userinfo") != null) {
+                userInfo = (Map<String, Object>) param.get("userinfo");
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "INFO",
+                        userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(), userInfo.get("msgTag").toString(), "User Login Message"));
+            } else {
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "ERROR", "", "", "", "", "User Login Message"));
+            }
+            if (JzbCheckParam.haveEmpty(param, new String[]{"cid", "projectid", "methodid"})) {
+                response = Response.getResponseError();
+            } else {
+                List<Map<String, Object>> list = tbCompanyMethodTargetService.queryMethodTarget(param);
+                PageInfo pageInfo = new PageInfo();
+                pageInfo.setList(list);
+                // 不做分页，返回总数
+                pageInfo.setTotal(list.size());
+                response = Response.getResponseSuccess(userInfo);
+                response.setPageInfo(pageInfo);
+            }
+        } catch (Exception ex) {
+            JzbTools.logError(ex);
+            response = Response.getResponseError();
+            logger.error(JzbLoggerUtil.getErrorLogger(userInfo == null ? "" : userInfo.get("msgTag").toString(), "add Company Method", ex.toString()));
+        }
+        if (userInfo != null) {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", flag ? "INFO" : "ERROR", userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(),
+                    userInfo.get("msgTag").toString(), "User Login Message"));
+        } else {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", "ERROR", "", "", "", "", "User Login Message"));
+        }
+        return response;
+    }
+
+    /**
+     * 点击达成方法论目标
+     *
+     * @param param
+     * @return
+     */
+    @RequestMapping(value = "/updateCompanyMethodTarget", method = RequestMethod.POST)
+    @ResponseBody
+    @Transactional
+    @CrossOrigin
+    public Response updateCompanyMethodTarget(@RequestBody Map<String, Object> param) {
+        Response response;
+        Map<String, Object> userInfo = null;
+        String api = "/operate/companyMethodTarget/updateCompanyMethodTarget";
+        boolean flag = true;
+        try {
+            if (param.get("userinfo") != null) {
+                userInfo = (Map<String, Object>) param.get("userinfo");
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "INFO",
+                        userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(), userInfo.get("msgTag").toString(), "User Login Message"));
+            } else {
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "ERROR", "", "", "", "", "User Login Message"));
+            }
+            if (JzbCheckParam.haveEmpty(param, new String[]{"list"})) {
+                response = Response.getResponseError();
+            } else {
+                List<Map<String, Object>> paramList = (List<Map<String, Object>>) param.get("list");
+                response = tbCompanyMethodTargetService.updateMethodTarget(paramList) > 0 ? Response.getResponseSuccess(userInfo) : Response.getResponseError();
+            }
+        } catch (Exception ex) {
+            JzbTools.logError(ex);
+            response = Response.getResponseError();
+            logger.error(JzbLoggerUtil.getErrorLogger(userInfo == null ? "" : userInfo.get("msgTag").toString(), "add Company Method", ex.toString()));
+        }
+        if (userInfo != null) {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", flag ? "INFO" : "ERROR", userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(),
+                    userInfo.get("msgTag").toString(), "User Login Message"));
+        } else {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", "ERROR", "", "", "", "", "User Login Message"));
+        }
+        return response;
+    }
+}
