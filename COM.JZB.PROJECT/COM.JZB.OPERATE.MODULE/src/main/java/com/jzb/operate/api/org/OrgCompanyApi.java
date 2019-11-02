c5cdef85b0f7b49789f8933b358c1494910e330d
hanbin <hanbin@hanbin.com>
2019-11-02 09:38:58 +0800

    czd help hanbin send to git

---
+++ COM.JZB.PROJECT/COM.JZB.OPERATE.MODULE/src/main/java/com/jzb/operate/api/org/OrgCompanyApi.java
@@ -0,0 +1,40 @@
+package com.jzb.operate.api.org;
+
+import com.jzb.base.message.Response;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
+
+/**
+ * @Description:
+ * @Author Han Bin
+ */
+@FeignClient(name = "jzb-org")
+@RequestMapping(value = "/org")
+public interface OrgCompanyApi {
+
+    /**
+     * 根据项目id获取项信息
+     *
+     * @author hanbin
+     * param projectid
+     */
+    @RequestMapping(value = "/getCompanyProjct", method = RequestMethod.POST)
+    @CrossOrigin
+    public Response getCompanyProjct(@RequestBody Map<String, Object> param);
+
+    /**
+     * 主页获取单位信息
+     *
+     * @author kuangbin
+     * para cid
+     */
+    @RequestMapping(value = "/getEnterpriseData", method = RequestMethod.POST)
+    @CrossOrigin
+    public Response getEnterpriseData(@RequestBody Map<String, Object> param);
+
+}
