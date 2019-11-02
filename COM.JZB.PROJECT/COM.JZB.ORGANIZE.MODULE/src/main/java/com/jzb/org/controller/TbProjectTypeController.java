e3bb3208088f579b5f0daa2416cdf78b868e50f6
chenduan <2514571823qq.com>
2019-11-02 09:16:57 +0800

    czd

--- COM.JZB.PROJECT/COM.JZB.ORGANIZE.MODULE/src/main/java/com/jzb/org/controller/TbProjectTypeController.java
+++ COM.JZB.PROJECT/COM.JZB.ORGANIZE.MODULE/src/main/java/com/jzb/org/controller/TbProjectTypeController.java
@@ -1,9 +1,12 @@
 package com.jzb.org.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.jzb.base.log.JzbLoggerUtil;
 import com.jzb.base.message.Response;
 import com.jzb.base.util.JzbTools;
 import com.jzb.org.service.TbProjectTypeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +25,11 @@ public class TbProjectTypeController {
     private TbProjectTypeService tbProjectTypeService;
 
     /**
+     * 日志记录对象
+     */
+    private final static Logger logger = LoggerFactory.getLogger(TbProjectTypeController.class);
+
+    /**
      * 获取公告类型
      * @param param
      * @return
@@ -31,15 +39,34 @@ public class TbProjectTypeController {
     @CrossOrigin
     public Response getProjectTypeList(@RequestBody(required = false) Map<String, Object> param){
         Response result;
+        Map<String, Object> userInfo = null;
+        String api = "/org/projectType/getProjectTypeList";
+        boolean flag = true;
         try {
+            // 如果获取参数userinfo不为空的话
+            if (param.get("userinfo") != null) {
+                userInfo = (Map<String, Object>) param.get("userinfo");
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "INFO",
+                        userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(), userInfo.get("msgTag").toString(), "User Login Message"));
+            } else {
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "ERROR", "", "", "", "", "User Login Message"));
+            }
             // 定义返回结果
             result = Response.getResponseSuccess();
             // 获取结果集
             List<Map<String, Object>> list = tbProjectTypeService.getProjectType();
             result.setResponseEntity(list);
         }catch (Exception ex){
+            flag = false;
             JzbTools.logError(ex);
             result=Response.getResponseError();
+            logger.error(JzbLoggerUtil.getErrorLogger(userInfo == null ? "" : userInfo.get("msgTag").toString(), "getProjectTypeList Method", ex.toString()));
+        }
+        if (userInfo != null) {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", flag ? "INFO" : "ERROR", userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(),
+                    userInfo.get("msgTag").toString(), "User Login Message"));
+        } else {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", "ERROR", "", "", "", "", "User Login Message"));
         }
         return result;
     }
