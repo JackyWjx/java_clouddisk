2e5462e8079d0f71bba86fdfb8b9bbc157ed8e22
dingshenchang <dingshenchang@dingshenchang.com>
2019-11-02 11:46:34 +0800

    uid

--- COM.JZB.PROJECT/COM.JZB.AUTH.MODULE/src/main/java/com/jzb/auth/service/AuthUserLoginService.java
+++ COM.JZB.PROJECT/COM.JZB.AUTH.MODULE/src/main/java/com/jzb/auth/service/AuthUserLoginService.java
@@ -243,6 +243,7 @@ public class AuthUserLoginService {
         if (b1 && b2) {
             List<Map> loList = userMapper.searchSendCode(map);
             if (loList.size() > 0) {
+                response.put("uid", loList.get(0).get("uid"));
                 response.put("code", "3");
             } else {
                 String uid = JzbRandom.getRandomCharCap(12);
