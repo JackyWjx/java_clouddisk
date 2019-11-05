91d68f1ecc907b2eaf7b22d230fc56c5bd18fe64
kuangbin <kuangbin@kuangbin.com>
2019-11-05 14:50:38 +0800

    增加方法

--- COM.JZB.PROJECT/COM.JZB.ORGANIZE.MODULE/src/main/java/com/jzb/org/dao/CompanyMapper.java
+++ COM.JZB.PROJECT/COM.JZB.ORGANIZE.MODULE/src/main/java/com/jzb/org/dao/CompanyMapper.java
@@ -337,4 +337,11 @@ public interface CompanyMapper {
      * @Author: Kuang Bin
      */
     int insertCompanySupplier(Map<String, Object> param);
+
+    /**
+     * CRM-销售业主-公海-供应商5
+     * 点击新建供应商建立单位下供应商
+     * @Author: Kuang Bin
+     */
+    int queryIsCompanySupplier(Map<String, Object> param);
 } // End interface CompanyMapper
