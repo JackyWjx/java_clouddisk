eccd1d2ec95827a1f7d47359f5a865025c940b08
chenduan <2514571823qq.com>
2019-11-05 18:19:24 +0800

    czd 添加日志记录

--- COM.JZB.PROJECT/COM.JZB.RESOURCE.MOUDLE/src/main/java/com/jzb/resource/controller/TbMethodTargetController.java
+++ COM.JZB.PROJECT/COM.JZB.RESOURCE.MOUDLE/src/main/java/com/jzb/resource/controller/TbMethodTargetController.java
@@ -1,11 +1,15 @@
 package com.jzb.resource.controller;
 
+import com.jzb.base.log.JzbLoggerUtil;
 import com.jzb.base.message.PageInfo;
 import com.jzb.base.message.Response;
 import com.jzb.base.util.JzbCheckParam;
 import com.jzb.base.util.JzbTools;
 import com.jzb.resource.service.TbMethodTargetService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,18 +28,37 @@ public class TbMethodTargetController {
     private TbMethodTargetService tbMethodTargetService;
 
     /**
+     * 日志记录对象
+     */
+    private final static Logger logger = LoggerFactory.getLogger(TbMethodDataController.class);
+
+    /**
      * 获取方法论目标
+     *
      * @param params
      * @return
      */
     @RequestMapping(value = "/getMethodTarget", method = RequestMethod.POST)
     @ResponseBody
+    @Transactional
+    @CrossOrigin
     public Response getMethodTarget(@RequestBody Map<String, Object> params) {
         Response result;
+        Map<String, Object> userInfo = null;
+        String api = "/methodTarget/getMethodTarget";
+        boolean flag = true;
         try {
+            if (params.get("userinfo") != null) {
+                userInfo = (Map<String, Object>) params.get("userinfo");
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "INFO",
+                        userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(), userInfo.get("msgTag").toString(), "User Login Message"));
+            } else {
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "ERROR", "", "", "", "", "User Login Message"));
+            }
             // 验证指定参数 为空则返回404 error
             if (JzbCheckParam.haveEmpty(params, new String[]{"dataid"})) {
                 result = Response.getResponseError();
+                logger.error(JzbLoggerUtil.getErrorLogger(userInfo == null ? "" : userInfo.get("msgTag").toString(), "getMethodTarget Method", "[param error] or [param is null]"));
             } else {
                 // 查询结果
                 List<Map<String, Object>> list = tbMethodTargetService.queryMethodTarget(params);
@@ -45,21 +68,28 @@ public class TbMethodTargetController {
 
                 // 定义返回response
                 // 定义返回结果
-                Map<String, Object> userInfo = (Map<String, Object>) params.get("userinfo");
                 result = Response.getResponseSuccess(userInfo);
                 result.setPageInfo(pi);
             }
         } catch (Exception e) {
+            flag = false;
             // 打印异常信息
             e.printStackTrace();
             result = Response.getResponseError();
+            logger.error(JzbLoggerUtil.getErrorLogger(userInfo == null ? "" : userInfo.get("msgTag").toString(), "getMethodTarget Method", e.toString()));
+        }
+        if (userInfo != null) {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", flag ? "INFO" : "ERROR", userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(),
+                    userInfo.get("msgTag").toString(), "User Login Message"));
+        } else {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", "ERROR", "", "", "", "", "User Login Message"));
         }
         return result;
     }
 
-
     /**
      * 获取方法论目标
+     *
      * @param params
      * @return
      */
@@ -67,10 +97,22 @@ public class TbMethodTargetController {
     @ResponseBody
     public Response getMethodTargetByTypeid(@RequestBody Map<String, Object> params) {
         Response result;
+        Map<String, Object> userInfo = null;
+        String api = "/methodTarget/getMethodTargetByTypeid";
+        boolean flag = true;
         try {
+            if (params.get("userinfo") != null) {
+                userInfo = (Map<String, Object>) params.get("userinfo");
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "INFO",
+                        userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(), userInfo.get("msgTag").toString(), "User Login Message"));
+            } else {
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "ERROR", "", "", "", "", "User Login Message"));
+            }
             // 验证指定参数 为空则返回404 error
             if (JzbCheckParam.haveEmpty(params, new String[]{"typeid"})) {
                 result = Response.getResponseError();
+                logger.error(JzbLoggerUtil.getErrorLogger(userInfo == null ? "" : userInfo.get("msgTag").toString(), "getMethodTargetByTypeid Method", "[param error] or [param is null]"));
+
             } else {
                 // 查询结果
                 List<Map<String, Object>> list = tbMethodTargetService.queryMethodTargetByTypeid(params);
@@ -80,73 +122,115 @@ public class TbMethodTargetController {
 
                 // 定义返回response
                 // 定义返回结果
-                Map<String, Object> userInfo = (Map<String, Object>) params.get("userinfo");
                 result = Response.getResponseSuccess(userInfo);
                 result.setPageInfo(pi);
             }
         } catch (Exception e) {
+            flag = false;
             // 打印异常信息
             e.printStackTrace();
             result = Response.getResponseError();
+            logger.error(JzbLoggerUtil.getErrorLogger(userInfo == null ? "" : userInfo.get("msgTag").toString(), "getMethodTargetByTypeid Method", e.toString()));
+        }
+        if (userInfo != null) {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", flag ? "INFO" : "ERROR", userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(),
+                    userInfo.get("msgTag").toString(), "User Login Message"));
+        } else {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", "ERROR", "", "", "", "", "User Login Message"));
         }
         return result;
     }
 
     /**
-     *  添加方法论目标
+     * 添加方法论目标
+     *
      * @param param
      * @return
      */
     @RequestMapping(value = "/addMethodTarget", method = RequestMethod.POST)
     @ResponseBody
     @CrossOrigin
+    @Transactional
     public Response addMethodTarget(@RequestBody Map<String, Object> param) {
         Response result;
+        Map<String, Object> userInfo = null;
+        String api = "/methodTarget/addMethodTarget";
+        boolean flag = true;
         try {
-
-            List<Map<String, Object>> list=(List<Map<String, Object>>) param.get("list");
+            if (param.get("userinfo") != null) {
+                userInfo = (Map<String, Object>) param.get("userinfo");
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "INFO",
+                        userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(), userInfo.get("msgTag").toString(), "User Login Message"));
+            } else {
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "ERROR", "", "", "", "", "User Login Message"));
+            }
+            List<Map<String, Object>> list = (List<Map<String, Object>>) param.get("list");
             // 调用添加目标的方法
 
             int count = tbMethodTargetService.addMethodTarget(list);
             if (count > 0) {
                 // 定义返回结果
-                Map<String, Object> userInfo = (Map<String, Object>) param.get("userinfo");
                 result = Response.getResponseSuccess(userInfo);
             } else {
                 result = Response.getResponseError();
+                logger.error(JzbLoggerUtil.getErrorLogger(userInfo == null ? "" : userInfo.get("msgTag").toString(), "addMethodTarget Method", "[sql add error]"));
             }
         } catch (Exception ex) {
+            flag = false;
             JzbTools.logError(ex);
             result = Response.getResponseError();
+            logger.error(JzbLoggerUtil.getErrorLogger(userInfo == null ? "" : userInfo.get("msgTag").toString(), "addMethodTarget Method", ex.toString()));
+        }
+        if (userInfo != null) {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", flag ? "INFO" : "ERROR", userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(),
+                    userInfo.get("msgTag").toString(), "User Login Message"));
+        } else {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", "ERROR", "", "", "", "", "User Login Message"));
         }
         return result;
     }
 
-
-
-    @RequestMapping(value = "/deleteMethodTarget",method = RequestMethod.POST)
+    @RequestMapping(value = "/deleteMethodTarget", method = RequestMethod.POST)
     @ResponseBody
     @CrossOrigin
-    public Response deleteMethod(@RequestBody Map<String, Object> param){
+    public Response deleteMethod(@RequestBody Map<String, Object> param) {
         Response result;
+        Map<String, Object> userInfo = null;
+        String api = "/methodTarget/deleteMethodTarget";
+        boolean flag = true;
         try {
-            int count=0;
-            List<Map<String, Object>> list=(List<Map<String, Object>>) param.get("list");
-            for (int i=0,l=list.size();i<l;i++){
-                list.get(i).put("status",2);
-                count= tbMethodTargetService.updateMethodTargetStatus(list.get(i));
+            if (param.get("userinfo") != null) {
+                userInfo = (Map<String, Object>) param.get("userinfo");
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "INFO",
+                        userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(), userInfo.get("msgTag").toString(), "User Login Message"));
+            } else {
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "ERROR", "", "", "", "", "User Login Message"));
+            }
+            int count = 0;
+            List<Map<String, Object>> list = (List<Map<String, Object>>) param.get("list");
+            for (int i = 0, l = list.size(); i < l; i++) {
+                list.get(i).put("status", 2);
+                count = tbMethodTargetService.updateMethodTargetStatus(list.get(i));
             }
             // 调用添加目标的方法
             if (count > 0) {
                 // 定义返回结果
-                Map<String, Object> userInfo = (Map<String, Object>) param.get("userinfo");
                 result = Response.getResponseSuccess(userInfo);
             } else {
                 result = Response.getResponseError();
+                logger.error(JzbLoggerUtil.getErrorLogger(userInfo == null ? "" : userInfo.get("msgTag").toString(), "deleteMethod Method", "[sql update error]"));
             }
         } catch (Exception ex) {
+            flag = false;
             JzbTools.logError(ex);
             result = Response.getResponseError();
+            logger.error(JzbLoggerUtil.getErrorLogger(userInfo == null ? "" : userInfo.get("msgTag").toString(), "deleteMethod Method", ex.toString()));
+        }
+        if (userInfo != null) {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", flag ? "INFO" : "ERROR", userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(),
+                    userInfo.get("msgTag").toString(), "User Login Message"));
+        } else {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", "ERROR", "", "", "", "", "User Login Message"));
         }
         return result;
     }
