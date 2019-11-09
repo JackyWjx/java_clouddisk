19950e9e180adc7858606be629e2c2987e16e038
Reed <reedhard@foxmail.com>
2019-11-09 21:07:59 +0800

    积分

---
+++ COM.JZB.PROJECT/COM.JZB.OPERATE.MODULE/src/main/java/com/jzb/operate/dao/TbScoreManualMapper.java
@@ -0,0 +1,23 @@
+package com.jzb.operate.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @描述
+ * @创建人 chenhui
+ * @创建时间 2019/11/9
+ * @修改人和其它信息
+ */
+@Mapper
+@Repository
+public interface TbScoreManualMapper {
+    //查询指导手册总数
+    Integer getCount(Map<String, Object> paramap);
+
+    //查询指导手册列表
+    List<Map<String, Object>> getScoreManual(Map<String, Object> paramap);
+}
