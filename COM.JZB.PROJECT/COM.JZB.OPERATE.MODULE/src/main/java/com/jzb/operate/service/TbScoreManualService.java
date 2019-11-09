072041476a524a715651eb2c07ddec2c71c35003
Reed <reedhard@foxmail.com>
2019-11-09 21:09:02 +0800

    积分

---
+++ COM.JZB.PROJECT/COM.JZB.OPERATE.MODULE/src/main/java/com/jzb/operate/service/TbScoreManualService.java
@@ -0,0 +1,29 @@
+package com.jzb.operate.service;
+
+import com.jzb.operate.dao.TbScoreManualMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+        *@描述
+        *@创建人
+        *@创建时间 2019/11/9
+        *@修改人和其它信息
+*/
+@Service
+public class TbScoreManualService {
+    @Autowired
+    TbScoreManualMapper scoreManual;
+
+    //查询积分指导手册总数
+    public Integer getCount(Map<String, Object> paramap) {
+        return scoreManual.getCount(paramap);
+    }
+
+    public List<Map<String, Object>> getActivity(Map<String, Object> paramap) {
+        return scoreManual.getScoreManual(paramap);
+    }
+}
