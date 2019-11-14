ad6823e2d7cacf45cbfa876b241f8705cbb88c86
chenduan <2514571823qq.com>
2019-11-14 17:37:21 +0800

    czd 添加日志记录

--- COM.JZB.PROJECT/COM.JZB.RESOURCE.MOUDLE/src/main/java/com/jzb/resource/controller/TbContractTemplateItemController.java
+++ COM.JZB.PROJECT/COM.JZB.RESOURCE.MOUDLE/src/main/java/com/jzb/resource/controller/TbContractTemplateItemController.java
@@ -1,11 +1,14 @@
 package com.jzb.resource.controller;
 
+import com.jzb.base.log.JzbLoggerUtil;
 import com.jzb.base.message.PageInfo;
 import com.jzb.base.message.Response;
 import com.jzb.base.util.JzbCheckParam;
 import com.jzb.base.util.JzbPageConvert;
 import com.jzb.base.util.JzbTools;
 import com.jzb.resource.service.TbContractTemplateItemService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +23,11 @@ public class TbContractTemplateItemController {
     private TbContractTemplateItemService tbContractTemplateItemService;
 
     /**
+     * 日志记录对象
+     */
+    private final static Logger logger = LoggerFactory.getLogger(TbContractTemplateItemController.class);
+
+    /**
      * 查询合同模板项
      *
      * @param param
@@ -31,12 +39,22 @@ public class TbContractTemplateItemController {
     @Transactional
     public Response getContractTemplateItem(@RequestBody Map<String, Object> param) {
         Response result;
+        Map<String, Object> userInfo = null;
+        String api = "/resource/contractTemplateItem/getContractTemplateItem";
+        boolean flag = true;
         try {
+            if (param.get("userinfo") != null) {
+                userInfo = (Map<String, Object>) param.get("userinfo");
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "INFO",
+                        userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(), userInfo.get("msgTag").toString(), "User Login Message"));
+            } else {
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "ERROR", "", "", "", "", "User Login Message"));
+            }
             // 判断指定参数为空则返回error
             if (JzbCheckParam.haveEmpty(param, new String[]{"tempid"})) {
                 result = Response.getResponseError();
+                logger.error(JzbLoggerUtil.getErrorLogger(userInfo == null ? "" : userInfo.get("msgTag").toString(), "getContractTemplateItem Method", "[param error] or [param is null]"));
             } else {
-
                 // 获取结果集
                 List<Map<String, Object>> list = tbContractTemplateItemService.quertContractTemplateItemByTempid(param);
 
@@ -50,8 +68,16 @@ public class TbContractTemplateItemController {
             }
 
         } catch (Exception ex) {
+            flag = false;
             JzbTools.logError(ex);
             result = Response.getResponseError();
+            logger.error(JzbLoggerUtil.getErrorLogger(userInfo == null ? "" : userInfo.get("msgTag").toString(), "getContractTemplateItem Method", ex.toString()));
+        }
+        if (userInfo != null) {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", flag ? "INFO" : "ERROR", userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(),
+                    userInfo.get("msgTag").toString(), "User Login Message"));
+        } else {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", "ERROR", "", "", "", "", "User Login Message"));
         }
         return result;
     }
@@ -68,26 +94,42 @@ public class TbContractTemplateItemController {
     @Transactional
     public Response getBaseContractTemplateItem(@RequestBody(required = false) Map<String, Object> param) {
         Response result;
+        Map<String, Object> userInfo = null;
+        String api = "/resource/contractTemplateItem/getBaseContractTemplateItem";
+        boolean flag = true;
         try {
-                param.put("tempid","000000000");
-                // 获取结果集
-                List<Map<String, Object>> list = tbContractTemplateItemService.quertContractTemplateItemByTempid(param);
+            if (param.get("userinfo") != null) {
+                userInfo = (Map<String, Object>) param.get("userinfo");
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "INFO",
+                        userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(), userInfo.get("msgTag").toString(), "User Login Message"));
+            } else {
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "ERROR", "", "", "", "", "User Login Message"));
+            }
+            param.put("tempid", "000000000");
+            // 获取结果集
+            List<Map<String, Object>> list = tbContractTemplateItemService.quertContractTemplateItemByTempid(param);
 
-                // 定义返回结果
-                Map<String, Object> userinfo = (Map<String, Object>) param.get("userinfo");
-                result = Response.getResponseSuccess(userinfo);
-                PageInfo pi = new PageInfo();
-                pi.setList(list);
-                pi.setTotal(list.size());
-                result.setPageInfo(pi);
+            // 定义返回结果
+            Map<String, Object> userinfo = (Map<String, Object>) param.get("userinfo");
+            result = Response.getResponseSuccess(userinfo);
+            PageInfo pi = new PageInfo();
+            pi.setList(list);
+            pi.setTotal(list.size());
+            result.setPageInfo(pi);
         } catch (Exception ex) {
+            flag=false;
             JzbTools.logError(ex);
             result = Response.getResponseError();
+            logger.error(JzbLoggerUtil.getErrorLogger(userInfo == null ? "" : userInfo.get("msgTag").toString(), "getBaseContractTemplateItem Method", ex.toString()));
+        }
+        if (userInfo != null) {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", flag ? "INFO" : "ERROR", userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(),
+                    userInfo.get("msgTag").toString(), "User Login Message"));
+        } else {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", "ERROR", "", "", "", "", "User Login Message"));
         }
         return result;
     }
-
-
 
     /**
      * 引用模板
@@ -101,12 +143,22 @@ public class TbContractTemplateItemController {
     @Transactional
     public Response getContractTemplateItemByid(@RequestBody Map<String, Object> param) {
         Response result;
+        Map<String, Object> userInfo = null;
+        String api = "/resource/contractTemplateItem/getContractTemplateItemByid";
+        boolean flag = true;
         try {
+            if (param.get("userinfo") != null) {
+                userInfo = (Map<String, Object>) param.get("userinfo");
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "INFO",
+                        userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(), userInfo.get("msgTag").toString(), "User Login Message"));
+            } else {
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "ERROR", "", "", "", "", "User Login Message"));
+            }
             // 判断指定参数为空则返回error
             if (JzbCheckParam.haveEmpty(param, new String[]{"itemid"})) {
                 result = Response.getResponseError();
+                logger.error(JzbLoggerUtil.getErrorLogger(userInfo == null ? "" : userInfo.get("msgTag").toString(), "getContractTemplateItemByid Method", "[param error] or [param is null]"));
             } else {
-
                 // 获取结果集
                 List<Map<String, Object>> list = tbContractTemplateItemService.quertContractTemplateItemByItemid(param);
 
@@ -120,14 +172,19 @@ public class TbContractTemplateItemController {
             }
 
         } catch (Exception ex) {
+            flag=false;
             JzbTools.logError(ex);
             result = Response.getResponseError();
+            logger.error(JzbLoggerUtil.getErrorLogger(userInfo == null ? "" : userInfo.get("msgTag").toString(), "getContractTemplateItemByid Method", ex.toString()));
+        }
+        if (userInfo != null) {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", flag ? "INFO" : "ERROR", userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(),
+                    userInfo.get("msgTag").toString(), "User Login Message"));
+        } else {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", "ERROR", "", "", "", "", "User Login Message"));
         }
         return result;
     }
-
-
-
 
     /**
      * 添加合同模板项
@@ -141,19 +198,38 @@ public class TbContractTemplateItemController {
     @Transactional
     public Response addContractTemplateItem(@RequestBody Map<String, Object> param) {
         Response result;
+        Map<String, Object> userInfo = null;
+        String api = "/resource/contractTemplateItem/addContractTemplateItem";
+        boolean flag = true;
         try {
+            if (param.get("userinfo") != null) {
+                userInfo = (Map<String, Object>) param.get("userinfo");
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "INFO",
+                        userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(), userInfo.get("msgTag").toString(), "User Login Message"));
+            } else {
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "ERROR", "", "", "", "", "User Login Message"));
+            }
             List<Map<String, Object>> list = (List<Map<String, Object>>) param.get("list");
             int count = tbContractTemplateItemService.addContractTemplateItem(list);
             if (count > 0) {
                 // 定义返回结果
-                Map<String, Object> userInfo = (Map<String, Object>) param.get("userinfo");
                 result = Response.getResponseSuccess(userInfo);
             } else {
                 result = Response.getResponseError();
+                logger.error(JzbLoggerUtil.getErrorLogger(userInfo == null ? "" : userInfo.get("msgTag").toString(), "getContractTemplateItemByid Method", "[addResult error]"));
             }
         } catch (Exception ex) {
+            flag=false;
             JzbTools.logError(ex);
             result = Response.getResponseError();
+            logger.error(JzbLoggerUtil.getErrorLogger(userInfo == null ? "" : userInfo.get("msgTag").toString(), "addContractTemplateItem Method", ex.toString()));
+
+        }
+        if (userInfo != null) {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", flag ? "INFO" : "ERROR", userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(),
+                    userInfo.get("msgTag").toString(), "User Login Message"));
+        } else {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", "ERROR", "", "", "", "", "User Login Message"));
         }
         return result;
     }
@@ -181,7 +257,5 @@ public class TbContractTemplateItemController {
         }
         return result;
     }
-
-
 
 }
--- COM.JZB.PROJECT/COM.JZB.RESOURCE.MOUDLE/src/main/java/com/jzb/resource/controller/TbContractTemplateTypeController.java
+++ COM.JZB.PROJECT/COM.JZB.RESOURCE.MOUDLE/src/main/java/com/jzb/resource/controller/TbContractTemplateTypeController.java
@@ -1,11 +1,14 @@
 package com.jzb.resource.controller;
 
+import com.jzb.base.log.JzbLoggerUtil;
 import com.jzb.base.message.PageInfo;
 import com.jzb.base.message.Response;
 import com.jzb.base.util.JzbCheckParam;
 import com.jzb.base.util.JzbPageConvert;
 import com.jzb.base.util.JzbTools;
 import com.jzb.resource.service.TbContractTemplateTypeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -21,9 +24,13 @@ import java.util.Map;
 @RequestMapping(value = "/resource/contracttemptype")
 public class TbContractTemplateTypeController {
 
-
     @Autowired
     private TbContractTemplateTypeService tbContactTemplateTypeService;
+
+    /**
+     * 日志记录对象
+     */
+    private final static Logger logger = LoggerFactory.getLogger(TbContractTemplateTypeController.class);
 
     /**
      * 获取合同模板分类
@@ -37,22 +44,41 @@ public class TbContractTemplateTypeController {
     @Transactional
     public Response getContractTemplateType(@RequestBody(required = false) Map<String, Object> param) {
         Response result;
+        Map<String, Object> userInfo = null;
+        String api = "/resource/contracttemptype/getContractTemplateType";
+        boolean flag = true;
         try {
+            if (param.get("userinfo") != null) {
+                userInfo = (Map<String, Object>) param.get("userinfo");
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "INFO",
+                        userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(), userInfo.get("msgTag").toString(), "User Login Message"));
+            } else {
+                logger.info(JzbLoggerUtil.getApiLogger(api, "1", "ERROR", "", "", "", "", "User Login Message"));
+            }
             if (JzbCheckParam.haveEmpty(param, new String[]{"pageno", "pagesize"})) {
                 result = Response.getResponseError();
+                logger.error(JzbLoggerUtil.getErrorLogger(userInfo == null ? "" : userInfo.get("msgTag").toString(), "getContractTemplateType Method", "[param error] or [param is null]"));
             } else {
                 JzbPageConvert.setPageRows(param);
                 List<Map<String, Object>> list = tbContactTemplateTypeService.getContactTemplateType(param);
-                Map<String, Object> userinfo=(Map<String, Object>) param.get("userinfo");
-                result=Response.getResponseSuccess(userinfo);
-                PageInfo pi=new PageInfo();
+                Map<String, Object> userinfo = (Map<String, Object>) param.get("userinfo");
+                result = Response.getResponseSuccess(userinfo);
+                PageInfo pi = new PageInfo();
                 pi.setList(list);
                 pi.setTotal(list.size());
                 result.setPageInfo(pi);
             }
         } catch (Exception ex) {
+            flag = false;
             JzbTools.logError(ex);
             result = Response.getResponseError();
+            logger.error(JzbLoggerUtil.getErrorLogger(userInfo == null ? "" : userInfo.get("msgTag").toString(), "getContractTemplateType Method", ex.toString()));
+        }
+        if (userInfo != null) {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", flag ? "INFO" : "ERROR", userInfo.get("ip").toString(), userInfo.get("uid").toString(), userInfo.get("tkn").toString(),
+                    userInfo.get("msgTag").toString(), "User Login Message"));
+        } else {
+            logger.info(JzbLoggerUtil.getApiLogger(api, "2", "ERROR", "", "", "", "", "User Login Message"));
         }
         return result;
     }
