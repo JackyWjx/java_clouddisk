c0e2f16f2b97a3f6524c9d5b112fca1c4ab5f0ff
kuangbin <kuangbin@kuangbin.com>
2019-11-20 10:00:58 +0800

    增加新公海类

---
+++ COM.JZB.PROJECT/COM.JZB.ORGANIZE.MODULE/src/main/java/com/jzb/org/controller/NewCompanyCommonController.java
@@ -0,0 +1,150 @@
+package com.jzb.org.controller;
+
+import com.jzb.base.data.JzbDataType;
+import com.jzb.base.message.PageInfo;
+import com.jzb.base.message.Response;
+import com.jzb.base.util.JzbCheckParam;
+import com.jzb.base.util.JzbRandom;
+import com.jzb.base.util.JzbTools;
+import com.jzb.org.service.CompanyService;
+import com.jzb.org.service.NewCompanyCommonService;
+import com.jzb.org.service.TbCompanyCommonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author kuangbin
+ */
+@RestController
+@RequestMapping(value = "/org/newCompanyCommon")
+public class NewCompanyCommonController {
+    @Autowired
+    private NewCompanyCommonService newCompanyCommonService;
+
+    @Autowired
+    private TbCompanyCommonService tbCompanyCommonService;
+
+    @Autowired
+    private CompanyService companyService;
+
+    @Autowired
+    private CompanyController companyController;
+    /**
+     * CRM-销售业主-公海-单位1
+     * 点击公海显示所有的单位信息
+     *
+     * @author kuangbin
+     */
+    @RequestMapping(value = "/getCompanyCommonList", method = RequestMethod.POST)
+    @CrossOrigin
+    public Response getCompanyCommonList(@RequestBody Map<String, Object> param) {
+        Response result;
+        try {
+            // 返回所有的企业列表
+            List<Map<String, Object>> companyList = newCompanyCommonService.getCompanyCommonList(param);
+            Map<String, Object> userInfo = (Map<String, Object>) param.get("userinfo");
+            result = Response.getResponseSuccess(userInfo);
+            PageInfo pageInfo = new PageInfo();
+            pageInfo.setList(companyList);
+            pageInfo.setTotal(companyList.size() == 0 ? 0 : JzbDataType.getInteger(companyList.get(0).get("count")));
+            result.setPageInfo(pageInfo);
+        } catch (Exception ex) {
+            JzbTools.logError(ex);
+            result = Response.getResponseError();
+        }
+        return result;
+    }
+
+    /**
+     * 管理员创建公海单位
+     *
+     * @param param
+     * @return com.jzb.base.message.Response
+     * @Author: Kuang Bin
+     * @DateTime: 2019/9/20 18:00
+     */
+    @RequestMapping(value = "/addCompanyCommonList", method = RequestMethod.POST)
+    @CrossOrigin
+    public Response addCompanyCommonList(@RequestBody Map<String, Object> param) {
+        Response result;
+        try {
+            String[] str = {"cid"};
+            if (JzbCheckParam.allNotEmpty(param, str)) {
+                Map<String, Object> userInfo = (Map<String, Object>) param.get("userinfo");
+                param.put("tcid", JzbRandom.getRandomCharCap(6));
+                param.put("status", "1");
+                param.put("addtime", System.currentTimeMillis());
+                param.put("uid", JzbDataType.getString(userInfo.get("uid")));
+                result = newCompanyCommonService.addCompanyCommonList(param) > 0 ?
+                        Response.getResponseSuccess(userInfo) : Response.getResponseError();
+            } else {
+                result = Response.getResponseError();
+            }
+        } catch (Exception e) {
+            JzbTools.logError(e);
+            result = Response.getResponseError();
+        }
+        return result;
+    }
+
+    /**
+     * CRM-销售业主-公海-单位3
+     * 点击修改按钮,进行公海单位修改
+     *
+     * @param param
+     * @return com.jzb.base.message.Response
+     * @Author: Kuang Bin
+     * @DateTime: 2019/10/11
+     */
+    @RequestMapping(value = "/modifyCompanyCommonList", method = RequestMethod.POST)
+    @CrossOrigin
+    public Response modifyCompanyCommonList(@RequestBody Map<String, Object> param) {
+        Response result;
+        try {
+            // 修改数据
+            int count = tbCompanyCommonService.modifyCompanyCommon(param);
+            // 获取用户信息
+            Map<String, Object> userInfo = (Map<String, Object>) param.get("userinfo");
+            param.put("upduid", JzbDataType.getString(userInfo.get("uid")));
+            if (count >= 1) {
+                result = Response.getResponseSuccess(userInfo);
+                Map<String, Object> map= companyService.getEnterpriseData(param);
+                companyController.comHasCompanyKey(map);
+                // 修改公海单位信息
+                newCompanyCommonService.modifyCompanyCommonList(param);
+            } else {
+                result = Response.getResponseError();
+            }
+        } catch (Exception e) {
+            //打印错误信息
+            JzbTools.logError(e);
+            result = Response.getResponseError();
+        }
+        return result;
+    }
+
+    /**
+     * CRM-销售业主-公海-单位4
+     * 点击删除按钮,进行公海单位删除
+     *
+     * @author kuangbin
+     */
+    @RequestMapping(value = "/removeCompanyCommonList", method = RequestMethod.POST)
+    @CrossOrigin
+    public Response removeCompanyCommonList(@RequestBody Map<String, Object> param) {
+        Response result;
+        try {
+            Map<String, Object> userInfo = (Map<String, Object>) param.get("userinfo");
+            // 返回结果值
+            result = newCompanyCommonService.removeCompanyCommonList(param) == 1 ?
+                    Response.getResponseSuccess(userInfo) : Response.getResponseError();
+        } catch (Exception ex) {
+            JzbTools.logError(ex);
+            result = Response.getResponseError();
+        }
+        return result;
+    }
+}
